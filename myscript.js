971450f56f052c6fa0be3b60e21d574eace58ed4
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2023-09-27 19:38:13 +0100

    linked more elements

--- myscript.js
+++ myscript.js
@@ -1,14 +1,12 @@
 const numButton = document.querySelectorAll(".num")
 const opButton = document.querySelectorAll(".op")
 const display = document.querySelector(".totalScreen")
-
-
+const clearButton = document.querySelector(".clearButton")
+const removeButton = document.querySelector(".removeButton")
 
 let runningTotal = ""
 let opState = ""
 let prevNumber = ""
-
-
 
 numButton.forEach((number)=>{
     number.addEventListener("click", ()=>{
@@ -24,9 +22,18 @@ opButton.forEach((operator)=>{
         display.innerHTML = ""
         runningTotal = ""
         display.innerHTML = operator.innerHTML;
-    })
-    
+    }); 
+});
+
+clearButton.addEventListener("click", ()=>{
+    console.log("Clear")
+});
+
+removeButton.addEventListener("click", ()=>{
+    console.log("Remove")
 })
+
+
 
 
 
