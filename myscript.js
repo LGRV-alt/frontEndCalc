980ec402d82b7e359516772f7c04850df0ce625b
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2023-09-28 19:43:12 +0100

    added function to the equals key

--- myscript.js
+++ myscript.js
@@ -3,40 +3,107 @@ const opButton = document.querySelectorAll(".op")
 const display = document.querySelector(".totalScreen")
 const clearButton = document.querySelector(".clearButton")
 const removeButton = document.querySelector(".removeButton")
+const equalsButton = document.querySelector(".equalsButton")
 
 let runningTotal = ""
 let opState = ""
 let prevNumber = ""
 
-function linkDisplay(){
-    display.innerHTML = runningTotal;
+
+const cal = {
+    add(a,b){
+        return a+b
+    },
+    sub(a,b){
+        return a-b
+    },
+    mul(a,b){
+        return a*b
+    },
+    div(a,b){
+        return a/b
+    },
+};
+
+function logValues(){
+    console.log(prevNumber, runningTotal, opState);
 }
 
+function linkDisplay(){
+    display.innerHTML = runningTotal;
+};
 
 function clearAll(){
     runningTotal = "";
     linkDisplay();
-}
+};
 
 function removeDigit(){
     runningTotal = runningTotal.slice(0,-1);
     linkDisplay();
+};
+
+function numInput(number){
+    let num = number.innerHTML;
+    runningTotal += num;
+    linkDisplay();
+    logValues();
+};
+
+function opInput(operator){
+    if(opState === ""){
+        prevNumber = runningTotal;
+        display.innerHTML = "";
+        runningTotal = ""
+        display.innerHTML = operator.innerHTML;
+        opState = operator.innerHTML;
+    } else{
+        display.innerHTML = operator.innerHTML;
+        opState = operator.innerHTML;
+    };
+    logValues();
+};
+
+function calculateTotal(num1, num2, operator){
+    let total
+    if(num1 !="" && num2 !=""){
+        newNum1 = parseFloat(num1);
+        newNum2 = parseFloat(num2);
+
+        switch(operator){
+            case "+":
+                total = cal.add(newNum1, newNum2);
+                break;
+
+            case "-":
+                total = cal.sub(newNum1, newNum2);
+                break;
+
+            case "/":
+                total = cal.div(newNum1, newNum2);
+                break;
+
+            case "*":
+                total = cal.mul(newNum1, newNum2);
+                break;
+        }
+        console.log(total);
+    }
+
+
 }
+
+
 
 numButton.forEach((number)=>{
     number.addEventListener("click", ()=>{
-        let num = number.innerHTML;
-        runningTotal += num;
-        display.innerHTML = runningTotal;        
+        numInput(number);      
     });
 });
 
 opButton.forEach((operator)=>{
     operator.addEventListener("click", ()=>{
-        display.innerHTML = prevNumber;
-        display.innerHTML = ""
-        runningTotal = ""
-        display.innerHTML = operator.innerHTML;
+        opInput(operator);
     }); 
 });
 
@@ -48,6 +115,10 @@ clearButton.addEventListener("click", ()=>{
 removeButton.addEventListener("click", ()=>{
     console.log("Remove")
     removeDigit();
+})
+
+equalsButton.addEventListener("click", ()=>{
+    calculateTotal(prevNumber, runningTotal, opState);
 })
 
 
